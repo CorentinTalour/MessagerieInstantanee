17056ca7e40935d1b315b8b6c96ec90b27b78ea7
Corentin Talour <CorentinTalour@gmail.com>
2024-12-20 16:55:04 +0100

    Je commit un truc :)

---
+++ MessagerieInstantanee/MessagerieInstantanee/Objects/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MessagerieInstantanee.Objects;
+
+public class User
+{
+    public string Id { get; set; }
+    public Data Data { get; set; }
+}
+
+public class Data
+{
+    public string Name { get; set; }
+}
